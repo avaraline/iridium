22aad525e8855fe4f16ebfed64a5ac7a867137cb
Dan Watson <dcwatson@gmail.com>
2023-06-29 23:00:34 -0400

    AQI option for weather, bump/pin requirements

--- iridium/commands/weather.py
+++ iridium/commands/weather.py
@@ -1,15 +1,30 @@
 import aiohttp
 
 ENDPOINT = "http://api.openweathermap.org/data/2.5/weather"
+AQI_ENDPOINT = "https://www.airnowapi.org/aq/observation/latLong/current/"
 
 
 def to_c(f):
     return (f - 32) * 5.0 / 9.0
 
 
-async def handle(message, *args, appid=None):
+def get_aqi(data):
+    max_aqi = 0
+    pollutant = "None"
+    condition = "Good"
+    for d in data:
+        if d["AQI"] < max_aqi:
+            continue
+        max_aqi = d["AQI"]
+        pollutant = d["ParameterName"]
+        condition = d["Category"]["Name"]
+    return "AQI = {} [{}] - {}".format(max_aqi, pollutant, condition)
+
+
+async def handle(message, *args, appid=None, airnow=None):
     if not appid:
         return
+
     params = {
         "units": "imperial",
         "appid": appid,
@@ -18,6 +33,7 @@ async def handle(message, *args, appid=None):
         params["zip"] = args[0]
     else:
         params["q"] = " ".join(args)
+
     async with aiohttp.ClientSession() as session:
         async with session.get(ENDPOINT, params=params) as resp:
             data = await resp.json()
@@ -39,4 +55,16 @@ async def handle(message, *args, appid=None):
                 unit,
                 data["weather"][0]["description"],
             )
-            await message.reply(weather)
+
+        if airnow:
+            params = {
+                "format": "application/json",
+                "latitude": data["coord"]["lat"],
+                "longitude": data["coord"]["lon"],
+                "API_KEY": airnow,
+            }
+            async with session.get(AQI_ENDPOINT, params=params) as resp:
+                aqi_data = await resp.json()
+                weather += " ({})".format(get_aqi(aqi_data))
+
+        await message.reply(weather)
